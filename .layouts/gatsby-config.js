d7695220ef59c4666c69e79ab8a919ef9d920649
Esantomi <pilinnasa@gmail.com>
2022-04-09 17:25:00 +0900

    from theowenyoung/wiki

---
+++ .layouts/gatsby-config.js
@@ -0,0 +1,107 @@
+const path = require("path");
+const pathPrefix = "/";
+const siteMetadata = {
+  title: "Temple of Haein : Janggyeong Panjeon",
+  shortName: "Wiki",
+  description:
+    "Personal wiki, backing storage.",
+  twitterName: "DH_haein",
+  imageUrl: "/graph-visualisation.jpg",
+  siteUrl: "https://wiki.haein.info",
+};
+module.exports = {
+  siteMetadata,
+  pathPrefix,
+  flags: {
+    DEV_SSR: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: `${__dirname}/..`,
+        ignore: [`**/\.*/**/*`],
+      },
+    },
+    {
+      resolve: "gatsby-theme-primer-wiki",
+      options: {
+        icon: "./static/logo.png",
+        sidebarComponents: ["latest", "tag"],
+        defaultIndexLatestPostCount: 15,
+        searchBody: true,
+        nav: [
+          {
+            title: "Latest",
+            url: "/latest/",
+          },
+
+          {
+            title: "Twitter",
+            url: "https://twitter.com/DH_haein",
+          },
+          {
+            title: "Home",
+            url: "https://haein.info/",
+          },
+          {
+            title: "About",
+            items: [
+              // {
+              //   title: "Newsletter",
+              //   url: "https://digests.owenyoung.com/",
+              // },
+              // {
+              //   title: "Now",
+              //   url: "https://www.owenyoung.com/now/",
+              // },
+              // {
+              //   title: "About",
+              //   url: "https://www.owenyoung.com/about/",
+              // },
+              {
+                title: "Github",
+                url: "https://github.com/Esantomi",
+              },
+              {
+                title: "Source Code",
+                url: "https://github.com/Esantomi/wiki",
+              },
+            ],
+          },
+        ],
+        editUrl: "https://github.com/Esantomi/wiki/tree/main/",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: siteMetadata.title,
+        short_name: siteMetadata.shortName,
+        start_url: pathPrefix,
+        background_color: `#f7f0eb`,
+        display: `standalone`,
+        icon: path.resolve(__dirname, "./static/logo.png"),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteMetadata.siteUrl,
+        sitemap: `${siteMetadata.siteUrl}/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-76F1LR70FY"],
+      },
+    },
+  ],
+};
---
+++ about.md
@@ -0,0 +1,12 @@
+---
+title: About Me and the Wiki
+tags:
+  - Me
+---
+
+## About the Wiki
+해인사 장경판전
+
+## About Me
+
+
